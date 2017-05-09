eb389b037fd1b8b3dc905f5f1da0184e273e2a9c
Julian Chan <julian.chan@chromatix.com.au>
2017-05-09 18:09:48 +1000

    separate templates from data, catch edge cases

--- server.go
+++ server.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+  "encoding/json"
+  "fmt"
+  "io/ioutil"
+  "log"
   "net/http"
   "os"
+  "strings"
 
   "github.com/urfave/negroni"
   "github.com/unrolled/render"
@@ -12,9 +17,8 @@ func main() {
   rend := render.New(render.Options{IsDevelopment: true})
   mux := http.NewServeMux()
 
-  mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-    rend.HTML(w, http.StatusOK, "index", "David")
-  })
+ 
+  handleRender(mux, rend)
 
   n := negroni.New()
   r := negroni.NewRecovery()
@@ -30,5 +34,60 @@ func main() {
     port = ":80"
   }
   addr := os.Getenv("SERVER_ADDR")
-  http.ListenAndServe( addr + port, n)
+  http.ListenAndServe( addr + port, n )
+}
+
+func handleRender(mux *http.ServeMux, rend *render.Render) {
+  mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+    defer func() {
+      if r := recover(); r != nil {
+        log.Println("recovered")
+        fmt.Fprintln(w, "Template Not Available")
+      }
+    }()
+    var data interface{}
+    templateName := strings.TrimPrefix(req.URL.Path, "/")
+    path := "/goapp/"
+    if templateName == "" {
+      templateName = "index"
+    } else {
+      templateName = strings.TrimSuffix(templateName, "/")
+    }
+    data, err := loadData(templateName + ".json", path)
+    if err != nil {
+      log.Println(err)
+      rend.HTML(w, http.StatusServiceUnavailable, "default/dataUnavailable", "")
+      return
+    }
+    templateExists := Exists(templateName)
+    if templateExists == false {
+      rend.HTML(w, http.StatusServiceUnavailable, "default/templateUnavailable", "")
+    } else {
+      rend.HTML(w, http.StatusOK, templateName, data)
+    }
+  })
+}
+
+func Exists(name string) bool {
+  filename := "templates/" + name + ".tmpl"
+  _, err := os.Stat( filename )
+  if os.IsNotExist(err) {
+    return false
+  }
+  return true
+}
+
+
+func loadData(filename string, path string) (interface{}, error) {
+  var raw []byte
+  raw, err := ioutil.ReadFile("data/" + filename)
+  if err != nil {
+    log.Println( err )
+    return raw, err
+  }
+  var data map[string]interface{}
+  err = json.Unmarshal(raw, &data)
+  data["Path"] = path
+
+  return data, err
 }
